cdb8478929fa27c191d3aecacc7013411edd9d63
talent330 <109216109+shouldperfect330@users.noreply.github.com>
2019-06-24 06:59:27 -0400

    This is the most recent version of the demo app for [The Angular Course](https://www.angularcourse.com).

--- src/app/judgelist/directives/JudgeTable.directive.js
+++ src/app/judgelist/directives/JudgeTable.directive.js
@@ -18,14 +18,15 @@
     }
   }
 
-  JudgeTableController.$inject = [];
+  JudgeTableController.$inject = ['firebaseDataService'];
 
-  function JudgeTableController() {
+  function JudgeTableController(firebaseDataService) {
     
     var vm = this;
 
     vm.removeJudge = removeJudge;
     vm.toggleDone = toggleDone;
+    vm.changePhoto = changePhoto;
 
     function removeJudge(Judge) {
       vm.judges.$remove(Judge);
@@ -35,6 +36,31 @@
       vm.judges.$save(Judge);
     }
 
+    function changePhoto(Judge) {
+
+      console.log("Judge", Judge);
+
+      $.FileDialog({multiple: true}).on('files.bs.filedialog', function(ev) {
+          var files = ev.files;
+          var text = "";
+          files.forEach(function(f) {
+              text += f.name + "<br/>";
+
+              var query = firebaseDataService.users.orderByChild("email").equalTo(Judge.email);
+
+              query.on('value', function(snapshot) {
+                  snapshot.forEach(function(weekSnapshot) {
+                      weekSnapshot.ref().update({ image: f.content });
+                  });
+              });
+          });
+          
+          $("#output").html(text);
+      }).on('cancel.bs.filedialog', function(ev) {
+          $("#output").html("Cancelled!");
+      });
+
+    }
   }
 
 })();
